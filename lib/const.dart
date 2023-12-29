c4af3cea5b173fa247fb4000fd293e2b0a66f5d6
Kartali-Mohamed <mohameekartali@gmail.com>
2023-12-29 09:24:25 +0100

    add new colors

--- lib/const.dart
+++ lib/const.dart
@@ -6,7 +6,9 @@ const color3 = const Color(0xFF24D37F);
 const color4 = const Color(0xFF353542);
 const color5 = const Color(0xFF2656FF);
 const color6 = const Color(0xFF666680);
-
+const color7 = const Color(0xFF2A2B34);
+const color8 = const Color(0xFF3B3C44);
+const color9 = const Color(0xFF959595);
 
 validInput(String val, int min, int max) {
   if (val.length > max) {
